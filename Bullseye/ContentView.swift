b7e12e57583ccea5b80f0e28ff3d48e1d32f44f5
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-11 19:19:27 -0600

    End part 1.

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,30 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+  //MARK: - PROPERTIES
   
   //MARK: - BODY
   var body: some View {
+    
     VStack {
       
       Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .kerning(2.0)
+        .multilineTextAlignment(.center)
+        .lineSpacing(4)
+        .font(.footnote)
       
       Text("89")
+        .kerning(-1.0)
+        .fontWeight(.black)
+        .font(.largeTitle)
       
       HStack {
+        
         Text("1")
+          .bold()
+        
         Slider(value: .constant(50), in: 1.0...100.0)
+        
         Text("100")
+          .bold()
       }
       
       Button(action: {
         
+        
       }, label: {
+        
         Text("Hit Me!")
       })
-      
     }
-    
-    
   }
 }
 
